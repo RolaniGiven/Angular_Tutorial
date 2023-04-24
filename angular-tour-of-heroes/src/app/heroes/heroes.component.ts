8b584143d1fb0fda83f6a76be0493f23106d5183
RolaniGiven <Rolanimaluleka00@gmail.com>
2023-04-24 05:07:27 +0200

    first commit

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent {
+     hero = {
+      id: 1,
+      name: 'Windstrom'
+    };
+    heroes = HEROES;
+    selectedHero?: Hero;
+    onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    } 
+
+}
